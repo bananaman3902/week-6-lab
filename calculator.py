ea661e4df17da18dda7caf41893a24cbe083016a
bananaman3902 <140997442+bananaman3902@users.noreply.github.com>
2024-08-30 10:25:45 +1200

    Exercise 1

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@ def add(x, y):
 def subtract(x, y):
     return x - y
 def multiply(x, y):
-    return x * y
+    return x / y
 def divide(x, y):
     if y == 0:
         raise ValueError("Cannot divide by zero.")
